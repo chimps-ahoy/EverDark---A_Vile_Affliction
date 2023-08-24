fcb35744548bb421f32db8dac4b8ff7b30ccbba9
Sebastien <sebastien.peterson.boudreau@gmail.com>
2023-08-24 12:37:21 -0300

    moved drawMenu()to TUI

--- src/main/java/ncg/everdark/runtime/Main.java
+++ src/main/java/ncg/everdark/runtime/Main.java
@@ -33,45 +33,7 @@ public class Main {
 			//e.printStackTrace();
 			System.exit(-1);
 		}
-		
-		UI ui = null;
-		if (legacy) {
-			System.out.println(drawMenu());
-			ui = new TUI();	
-		} else {
-			ui = new GUI(CFG.getWidth(), CFG.getHeight());
-		}
+		UI ui = (legacy) ? new TUI() : new GUI(CFG.getWidth(), CFG.getHeight());
 		SwingUtilities.invokeLater(ui::start);	
-	}
-
-	public static String drawMenu() {
-		String menu = "";
-		String title = "EverDark";
-		String subtitle = "---A Vile Affliction---";
-		String VERS = "v0.1.4G";
-		int WIDTH = CFG.getWidth();
-		int HEIGHT = CFG.getHeight();
-		for (int i = 0; i < HEIGHT-2; i++) {
-			for (int j = 0; j < WIDTH; j++) {
-				if (i == (HEIGHT-2)/2 && j == (WIDTH-title.length())/2) {
-					menu += title;
-					j += title.length()-1;
-				} else if (i == (HEIGHT-2)/2 + 1 && j == (WIDTH-subtitle.length())/2) {
-					menu += subtitle;
-					j += subtitle.length()-1;
-				} else if (i == HEIGHT-3 && j == WIDTH-VERS.length()-1) {
-					menu += VERS;
-					j += VERS.length()-1;
-				} else if (i == 0 || (i == HEIGHT-3 && j > 0 && j < WIDTH-1)) {
-					menu += '_';
-				} else if (j == 0 || j == WIDTH-1) {
-					menu += '|';
-				} else {
-					menu += " ";
-				}
-			}
-			menu += '\n';
-		}
-		return menu + "\nWould you like to load from file? (Y/N): ";
 	}
 }
--- src/main/java/ncg/everdark/ui/TUI.java
+++ src/main/java/ncg/everdark/ui/TUI.java
@@ -79,11 +79,44 @@ public class TUI extends UI {
 	}
 
 	public void start() {
+		System.out.println(drawMenu());
+
 		Scanner in = new Scanner(System.in);
 		while (super.acceptingInput) {
 			handle(in.nextLine());
 		}
 		System.exit(0);
+	}
+
+	private static String drawMenu() {
+		String menu = "";
+		String title = "EverDark";
+		String subtitle = "---A Vile Affliction---";
+		String VERS = "v0.1.4G";
+		int WIDTH = CFG.getWidth();
+		int HEIGHT = CFG.getHeight();
+		for (int i = 0; i < HEIGHT-2; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				if (i == (HEIGHT-2)/2 && j == (WIDTH-title.length())/2) {
+					menu += title;
+					j += title.length()-1;
+				} else if (i == (HEIGHT-2)/2 + 1 && j == (WIDTH-subtitle.length())/2) {
+					menu += subtitle;
+					j += subtitle.length()-1;
+				} else if (i == HEIGHT-3 && j == WIDTH-VERS.length()-1) {
+					menu += VERS;
+					j += VERS.length()-1;
+				} else if (i == 0 || (i == HEIGHT-3 && j > 0 && j < WIDTH-1)) {
+					menu += '_';
+				} else if (j == 0 || j == WIDTH-1) {
+					menu += '|';
+				} else {
+					menu += " ";
+				}
+			}
+			menu += '\n';
+		}
+		return menu + "\nWould you like to load from file? (Y/N): ";
 	}
 
 }
